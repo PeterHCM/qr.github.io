35ca3ce84cca9b020e43920db358525630cbfb69
Peter <90168237+PeterHCM@users.noreply.github.com>
2025-07-12 01:38:56 +0700

    Update vtlqr.js

--- scripts/vtlqr.js
+++ scripts/vtlqr.js
@@ -59,7 +59,7 @@ function saveData() {
     alert("Vui lòng nhập đầy đủ thông tin trước khi lưu.");
     return;
   }
-
+	sendToGoogleForm(po, model, date, time, serial);
   const row = document.createElement('tr');
   row.innerHTML = `
     <td>${sttCounter++}</td>
@@ -78,3 +78,24 @@ saveButton.addEventListener('click', () => {
   updateDateTime();
   saveData();
 });
+// Luu Google Form
+function sendToGoogleForm(po, model, date, time, serial) {
+  const formURL = "https://docs.google.com/forms/d/e/1FAIpQLSfv2eU5P0e8qFU42ORx9FFshzqIC-_XVzViDnhw4fqV6bOGzg/formResponse";
+
+  const formData = new FormData();
+  formData.append("entry.2005620554", po);     // Số PO
+  formData.append("entry.1045781291", model);  // Model
+  formData.append("entry.1166974658", date);   // Ngày
+  formData.append("entry.1065046570", time);   // Giờ
+  formData.append("entry.839337160", serial);  // Serial Number
+
+  fetch(formURL, {
+    method: "POST",
+    mode: "no-cors",
+    body: formData
+  }).then(() => {
+    console.log("✅ Đã gửi dữ liệu lên Google Form");
+  }).catch(err => {
+    console.error("❌ Lỗi gửi dữ liệu:", err);
+  });
+}
